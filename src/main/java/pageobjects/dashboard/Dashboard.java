6833bb040ccadb31f120307c105f877bd19e2c54
Rajnish Pradhan <Rajnish Pradhan>
2017-02-12 16:29:31 +0545

    working in dashboard

--- src/main/java/pageobjects/dashboard/Dashboard.java
+++ src/main/java/pageobjects/dashboard/Dashboard.java
@@ -1,7 +1,15 @@
 package pageobjects.dashboard;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 /**
  * Created by RajniSh on 2/12/2017.
  */
 public class Dashboard {
+    WebDriver driver;
+
+    @FindBy(xpath = ".//*[@id='menu-dashboard']/a/div[3]")
+    WebElement dashboardLink;
 }
