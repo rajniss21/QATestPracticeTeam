44dd3ad0a9bd295ca06ae9f14ad6716ace495d73
Rajnish Pradhan <Rajnish Pradhan>
2017-02-12 16:45:45 +0545

    working on dashboard

--- src/test/java/tests/dashboardTest/DashboardTest.java
+++ src/test/java/tests/dashboardTest/DashboardTest.java
@@ -1,7 +1,31 @@
 package tests.dashboardTest;
 
+import org.junit.Before;
+import org.junit.Test;
+import pageobjects.dashboard.Dashboard;
+import tests.BaseTestLogin;
+
 /**
  * Created by RajniSh on 2/12/2017.
  */
-public class DashboardTest {
+public class DashboardTest extends BaseTestLogin{
+    private Dashboard dashboard;
+    @Before
+    public void setup(){
+        super.setup();
+        dashboard=new Dashboard(driver);
+    }
+    @Test
+    public void testThatDashboardLinkWorks(){
+        dashboard.gotoDashboardLink();
+    }
+    @Test
+    public void testThatHomeLinkWorks(){
+        dashboard.gotoHomeLink();
+    }
+    @Test
+    public void testThatVisitSiteLinkWorks(){
+        dashboard.gotoVisitSiteLink();
+    }
+
 }
