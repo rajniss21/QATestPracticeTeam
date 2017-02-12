4e57152e242e31df3738d38812c805c6ed9e00d6
Rajnish Pradhan <Rajnish Pradhan>
2017-02-12 16:39:11 +0545

    working on dashboard

--- src/main/java/pageobjects/dashboard/Dashboard.java
+++ src/main/java/pageobjects/dashboard/Dashboard.java
@@ -2,7 +2,10 @@ package pageobjects.dashboard;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * Created by RajniSh on 2/12/2017.
@@ -12,4 +15,22 @@ public class Dashboard {
 
     @FindBy(xpath = ".//*[@id='menu-dashboard']/a/div[3]")
     WebElement dashboardLink;
+    @FindBy(xpath = ".//*[@id='menu-dashboard']/ul/li[2]/a")
+    WebElement homeLink;
+    @FindBy(xpath = ".//*[@id='wp-admin-bar-view-site']/a")
+    WebElement visitSite;
+
+    public Dashboard(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+    public void gotoDashboardLink(){
+        new Actions(driver).moveToElement(dashboardLink).build().perform();
+    }
+    public void gotoHomeLink(){
+        new Actions(driver).moveToElement(homeLink).build().perform();
+    }
+    public void gotoVisitSiteLink(){
+        new Actions(driver).moveToElement(visitSite).build().perform();
+    }
 }
---
+++ src/test/java/tests/dashboardTest/DashboardTest.java
@@ -0,0 +1,7 @@
+package tests.dashboardTest;
+
+/**
+ * Created by RajniSh on 2/12/2017.
+ */
+public class DashboardTest {
+}
