ef9e378e39c6e3945e3a144c5ec51153a71eec2d
Rajnish Pradhan <Rajnish Pradhan>
2017-02-12 17:24:11 +0545

    working on pages

--- src/test/java/tests/pagesTest/PagesTest.java
+++ src/test/java/tests/pagesTest/PagesTest.java
@@ -1,7 +1,23 @@
 package tests.pagesTest;
 
+import org.junit.Test;
+import pageobjects.dashboard.Dashboard;
+import pageobjects.pages.AddNewPage;
+import tests.BaseTestLogin;
+
 /**
  * Created by RajniSh on 2/12/2017.
  */
-public class PagesTest {
+public class PagesTest extends BaseTestLogin {
+    @Test
+    public void testThatAddNewPageWorks(){
+        Dashboard dashboard=new Dashboard(driver);
+        dashboard.gotoPagesLink();
+        dashboard.gotoAddNewPageLink();
+
+        AddNewPage addNewPage=new AddNewPage(driver);
+        addNewPage.doAddTitle("software qa meetup");
+        addNewPage.addParagraph("how qa fails");
+        addNewPage.clickPublishButton();
+    }
 }
