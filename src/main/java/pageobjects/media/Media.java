bc9033186bb904da80cade627ec6c6c7aa91e951
ujjwal atreya <ujjwal atreya>
2017-02-12 18:06:59 +0545

    media class

---
+++ src/main/java/pageobjects/media/Media.java
@@ -0,0 +1,14 @@
+package pageobjects.media;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Ujjwal on 2/12/2017.
+ */
+public class Media {
+
+
+}
